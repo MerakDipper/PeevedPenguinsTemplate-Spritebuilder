13c37f672305c34048cf26a8c646edc0c8df08fd
张璇 <zhangxuan@zhangxuandeMacBook-Pro.local>
2015-01-25 00:43:38 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by 张璇 on 15/1/25.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
