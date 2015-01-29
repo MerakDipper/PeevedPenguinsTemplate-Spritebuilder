537d0214489e7c0ba3cf9f07e5cee196baf7bb77
张璇 <zhangxuan@zhangxuandeMacBook-Pro.local>
2015-01-29 01:10:48 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by 张璇 on 15/1/29.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
